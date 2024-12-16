3a27809e909c1f1b2f39083aff35cf1603f22891
IamYubrajKhatri <khatriyubraj1157@gmail.com>
2024-12-16 18:46:09 +0100

    azure

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:"http://localhost:4000",
+        target:"https://setflixbackend-g5cyc7bzarh9cngu.germanywestcentral-01.azurewebsites.net/",
       }
     }
   }
